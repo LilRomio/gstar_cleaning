5ef943519cb45b3c5b1c02f57aace02566b40aff
LilRomio <popescu.romeo9@gmail.com>
2023-08-28 12:48:32 +0300

    2.1 update bugs fix navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,28 +12,8 @@ const Navbar = () => {
     <nav className="app__navbar">
       <div className="app__navbar-logo">
         <img src={images.logo2} alt="logo" />
-        <div
-          className="app__flex"
-          style={{ flexDirection: 'column', alignItems: 'flex-start' }}
-        >
-          <div className="app__navbar-contact p-text">
-            <BiMobileVibration className="app__contact-logo" />
-            <a className="p-text" href="tel:0733273674 ">
-              0733273674{' '}
-            </a>
-            /
-            <a className="p-text" href="tel:0734540243 ">
-              0734540243{' '}
-            </a>
-          </div>
-          <div className=" app__navbar-contact  p-text">
-            <BiLogoGmail className="app__contact-logo" />
-            <a className="p-text" href="mailto:giurgealena@gmail.com">
-              giurgealena@gmail.com
-            </a>
-          </div>
-        </div>
       </div>
+
       <ul className="app__navbar-links">
         {['acasa', 'servicii', 'preturi', 'contact'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
@@ -42,7 +22,27 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
-
+      <div
+        className="app__flex"
+        style={{ flexDirection: 'column', alignItems: 'flex-start' }}
+      >
+        <div className="app__navbar-contact p-text">
+          <BiMobileVibration className="app__contact-logo" />
+          <a className="p-text" href="tel:0733273674 ">
+            0733273674{' '}
+          </a>
+          /
+          <a className="p-text" href="tel:0734540243 ">
+            0734540243{' '}
+          </a>
+        </div>
+        <div className=" app__navbar-contact  p-text">
+          <BiLogoGmail className="app__contact-logo" />
+          <a className="p-text" href="mailto:giurgealena@gmail.com">
+            giurgealena@gmail.com
+          </a>
+        </div>
+      </div>
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
 

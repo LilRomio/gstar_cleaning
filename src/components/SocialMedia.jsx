4b4a9db49aed94dc2660ebc790ab29d15840b11e
LilRomio <popescu.romeo9@gmail.com>
2023-08-28 15:02:14 +0300

    update 2.3 socials on mobile

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,21 +1,65 @@
 import React from 'react';
 import { FaFacebookF, FaWhatsappSquare } from 'react-icons/fa';
 import { BiLogoGmail, BiMobileVibration } from 'react-icons/bi';
-
+import { motion } from 'framer-motion';
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <BiMobileVibration />
+        <a href="tel:0733273674 " target="_blank" rel="noreferrer">
+          <motion.div
+            whileInView={{ scale: [0, 1] }}
+            whileHover={{ scale: [1, 0.9] }}
+            transition={{ duration: 0.25 }}
+            className="app__flex"
+          >
+            <BiMobileVibration />
+          </motion.div>
+        </a>
       </div>
       <div>
-        <FaWhatsappSquare />
+        <a
+          href="whatsapp://send?text=Hello World!&phone=+40733273674"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <motion.div
+            whileInView={{ scale: [0, 1] }}
+            whileHover={{ scale: [1, 0.9] }}
+            transition={{ duration: 0.25 }}
+            className="app__flex"
+          >
+            <FaWhatsappSquare />
+          </motion.div>
+        </a>
       </div>
       <div>
-        <FaFacebookF />
+        <a
+          href="https://www.facebook.com/people/Gstar-cleaning-property-management/100090615077315/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <motion.div
+            whileInView={{ scale: [0, 1] }}
+            whileHover={{ scale: [1, 0.9] }}
+            transition={{ duration: 0.25 }}
+            className="app__flex"
+          >
+            <FaFacebookF />
+          </motion.div>
+        </a>
       </div>
       <div>
-        <BiLogoGmail />
+        <a href="mailto:giurgealena@gmail.com" target="_blank" rel="noreferrer">
+          <motion.div
+            whileInView={{ scale: [0, 1] }}
+            whileHover={{ scale: [1, 0.9] }}
+            transition={{ duration: 0.25 }}
+            className="app__flex"
+          >
+            <BiLogoGmail />
+          </motion.div>
+        </a>
       </div>
     </div>
   );
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import './Footer.scss';
 import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
+import { FaFacebookF, FaWhatsappSquare } from 'react-icons/fa';
+import { BiLogoGmail, BiMobileVibration } from 'react-icons/bi';
+import { motion } from 'framer-motion';
 
 const Footer = () => {
   const [formData, setFormData] = useState({
@@ -58,6 +61,40 @@ const Footer = () => {
           {''}/{''}
           <a href="tel:0734540243 " className="p-text">
             0734540243
+          </a>
+        </div>
+      </div>
+      <div className="app__social-footer">
+        <div>
+          <a
+            href="whatsapp://send?text=Hello World!&phone=+40733273674"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <motion.div
+              whileInView={{ scale: [0, 1] }}
+              whileHover={{ scale: [1, 0.9] }}
+              transition={{ duration: 0.25 }}
+              className="app__flex"
+            >
+              <FaWhatsappSquare />
+            </motion.div>
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://www.facebook.com/people/Gstar-cleaning-property-management/100090615077315/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <motion.div
+              whileInView={{ scale: [0, 1] }}
+              whileHover={{ scale: [1, 0.9] }}
+              transition={{ duration: 0.25 }}
+              className="app__flex"
+            >
+              <FaFacebookF />
+            </motion.div>
           </a>
         </div>
       </div>
